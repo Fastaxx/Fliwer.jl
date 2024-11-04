c1c2d2ed7c7c0142821188180dc978d45b94b0a0
Fastaxx <Fastaxx@github.com>
2024-11-04 14:15:50 +0100

    Add Example doc

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,5 @@
 using Documenter, Fliwer
 
-makedocs(sitename="Fliwer Documentation", authors = "Louis Libat")
+makedocs(sitename="Fliwer Documentation", authors = "Louis Libat", pages= ["index.md", "Examples" => "examples.md"])
 
 deploydocs(repo = "github.com/Fastaxx/Fliwer.jl.git", target = "build", branch = "gh-pages", push_preview = false)
---
+++ docs/src/examples.md
@@ -0,0 +1,13 @@
+## Example
+
+### Steady Diffusion Monophasic
+
+### Steady Diffusion Diphasic
+
+### Unsteady Diffusion Monophasic
+
+### Unsteady Diffusion Diphasic
+
+Notes : 
+- Use GMRES with : abstol=1e-15, restart=60
+- 
