d0ab566bda72cbc3417751aba2bff8ccbe7f0c56
Fastaxx <l.libat2001@gmail.com>
2025-02-11 18:34:23 +0100

    Steph Full  1 2 time steps

--- examples/1D/Moving/NonPrescribed/Stefan_1ph.jl
+++ examples/1D/Moving/NonPrescribed/Stefan_1ph.jl
@@ -16,11 +16,12 @@ x = range(x0, stop = lx, length = nx+1)
 
 Δt = 0.5 * (lx/nx)^2
 Tend = 1.0
-nt = 2
+nt = 3
 t = [i*Δt for i in 0:nt]
 
 # Define the body
 xf = 0.02*lx   # Interface position
+@show xf
 initial_body = Body((x,_=0)->(x - xf), (x,_)->(x), domain, false)  # Initial body
 body = Body((x,t, _=0)->(x - xf), (x,)->(x,), domain, false)
 
@@ -54,144 +55,6 @@ u0ₒ = zeros((nx+1))
 u0ᵧ = zeros((nx+1))
 u0 = vcat(u0ₒ, u0ᵧ)
 
-@show u0
 # Define the solver
 s = MotionDiffusionUnsteadyMono(Fluide, bc_b, bc, Δt, Tend, u0, "BE")
 solve_MotionDiffusionUnsteadyMono!(s, Fluide, u0, Δt, Tend, nt, bc_b, bc, body, mesh, t, "BE", xf)
-
-readline()
-# Set iteration parameters 
-max_iter = 100
-tol = 1e-20
-
-old_xf = xf
-iter   = 0
-err    = Inf
-
-function run_stefan_iteration!(
-    s,
-    body::Body,
-    mesh::CartesianMesh,
-    spaceTimeMesh::CartesianSpaceTimeMesh,
-    capacity::Capacity,
-    operator::SpaceTimeOps,
-    Fluide::Phase,
-    bc_b::BorderConditions,
-    bc::AbstractBoundary,
-    u0::Vector{Float64},
-    domain,
-    ρ::Float64,
-    L::Float64,
-    max_iter::Int,
-    tol::Float64,
-    Δt::Float64,
-    Tend::Float64,
-    xf::Float64
-)
-    residuals = Float64[]  # Store residual at each iteration
-    err::Float64 = Inf
-    old_xf::Float64 = xf
-    iter::Int = 0
-
-    while (iter < max_iter) && (err > tol)
-        iter += 1
-
-        # 1) Solve the linear system
-        A_reduced, b_reduced, rows_idx, cols_idx = remove_zero_rows_cols!(s.A, s.b)
-        x_reduced = A_reduced \ b_reduced
-        s.x = zeros(size(s.A, 1))
-        s.x[cols_idx] .= x_reduced
-        Tᵢ = s.x
-
-        @show Tᵢ
-        # 2) Update volumes / compute new interface
-        Vn_1 = capacity.A[2][1:end÷2, 1:end÷2]
-        Vn   = capacity.A[2][end÷2+1:end, end÷2+1:end]
-        Hₙ   = sum(diag(Vn))
-        Hₙ₊₁ = sum(diag(Vn_1))
-
-        # Compute flux
-        nx = Fluide.operator.size[1]
-        W! = operator.Wꜝ[1:nx, 1:nx]
-        G = operator.G[1:nx, 1:nx]
-        H = operator.H[1:nx, 1:nx]
-        V = operator.V[1:nx, 1:nx]
-        Tₒ, Tᵧ = Tᵢ[1:nx], Tᵢ[nx+1:end]
-        Interface_term = H' * W! * G * Tₒ + H' * W! * H * Tᵧ
-        Interface_term = -1/(ρ*L) * sum(Interface_term)
-        @show Interface_term
-        # New interface position
-        res = Hₙ₊₁ - Hₙ - Interface_term
-        @show -diag(Vn_1 - Vn) * ρ * L - H' * W! * G * Tₒ + H' * W! * H * Tᵧ
-        @show res
-        @show Hₙ₊₁ - Hₙ
-        xf_new = Hₙ + Interface_term
-        
-        err = abs(xf_new - old_xf)
-        println("Iteration $iter | xf = $xf_new | error = $err")
-
-        # Store residuals
-        push!(residuals, err)
-
-        # 3) Update geometry if not converged
-        if err <= tol
-            break
-        end
-        old_xf = xf_new
-
-        # Store tn+1 and tn
-        tn1 = t[2]
-        tn  = t[1]
-
-        # Rebuild domain : # Add t interpolation : x - (xf*(tn1 - t)/(\Delta t) + xff*(t - tn)/(\Delta t))
-        body = Body((xx,t, _=0)->(xx - (xf*(tn1 - t)/Δt + xf_new*(t - tn)/Δt)), (xx,)->(xx,), domain, false)
-        #identify!(mesh, body)
-        spaceTimeMesh.tag = mesh.tag
-
-        capacity = Capacity(body, spaceTimeMesh)
-        operator = SpaceTimeOps(capacity.A, capacity.B, capacity.V, capacity.W, (nx, 2))
-        Fluide   = Phase(capacity, operator, (x,y,z,t)->0.0, 1.0)
-
-        s = MotionDiffusionUnsteadyMono(Fluide, bc_b, bc, Δt, Tend, Tᵢ, "BE")
-
-        # recompute the linear system
-        A_reduced, b_reduced, rows_idx, cols_idx = remove_zero_rows_cols!(s.A, s.b)
-        x_reduced = A_reduced \ b_reduced
-        s.x = zeros(size(s.A, 1))
-        s.x[cols_idx] .= x_reduced
-        Tᵢ = s.x
-    end
-
-    if err <= tol
-        println("Converged after $iter iterations with xf = $xf, error = $err")
-    else
-        println("Reached max_iter = $max_iter with xf = $xf, error = $err")
-    end
-
-    return xf,residuals
-end
-
-xf_final, residuals = run_stefan_iteration!(
-     s, body, mesh, spaceTimeMesh, capacity,
-     operator, Fluide, bc_b, bc, u0, domain,
-     ρ, L, max_iter, tol, Δt, Tend, xf )
-
-
-
-using CairoMakie
-
-fig = Figure()
-ax  = Axis(fig[1,1], xlabel = "Iteration", ylabel = "Residual", title = "Residuals vs. Iteration")
-lines!(ax, 1:length(residuals), log10.(residuals), color=:blue)
-display(fig)
-"""
-Vin = [Vn[i, i] for i in 1:size(Vn, 1)]
-Vin1 = [Vn_1[i, i] for i in 1:size(Vn_1, 1)]
-
-fig = Figure(size = (800, 400))
-ax = Axis(fig[1, 1], xlabel = "x", ylabel = "V", title = "Vn and Vn+1")
-scatter!(ax, x, Vin, color = :blue, label = "Vn")
-scatter!(ax, x, Vin1, color = :red, label = "Vn+1")
-axislegend(ax, position = :rt)
-display(fig)
-"""
--- src/NonPrescribedMovingSolver.jl
+++ src/NonPrescribedMovingSolver.jl
@@ -33,13 +33,13 @@ function solve_MotionDiffusionUnsteadyMono!(s::Solver, phase::Phase, Tᵢ::Vecto
     # Params
     nx, _ = phase.operator.size
     ρ, L = 1.0, 1.0
-    max_iter, tol = 100, 1e-20
+    max_iter, tol = 100, 1e-10
     err = Inf
     old_xf = xf
     iter = 0
     residuals = Float64[]
 
-    # First time step : Newton 
+    # First time step : Newton to compute the interface position xf1
     while (iter < max_iter) && (err > tol)
         iter += 1
 
@@ -76,7 +76,7 @@ function solve_MotionDiffusionUnsteadyMono!(s::Solver, phase::Phase, Tᵢ::Vecto
 
         # New interface position
         res = Hₙ₊₁ - Hₙ - Interface_term
-        xf_new = Hₙ + Interface_term
+        global xf_new = Hₙ + Interface_term
         err = abs(xf_new - old_xf)
         println("Iteration $iter | xf = $xf_new | error = $err")
 
@@ -100,23 +100,139 @@ function solve_MotionDiffusionUnsteadyMono!(s::Solver, phase::Phase, Tᵢ::Vecto
 
         capacity = Capacity(body, spaceTimeMesh)
         operator = SpaceTimeOps(capacity.A, capacity.B, capacity.V, capacity.W, (nx, 2))
-        Fluide   = Phase(capacity, operator, (x,y,z,t)->0.0, 1.0)
+        phase   = Phase(capacity, operator, (x,y,z,t)->0.0, 1.0)
 
-        s = MotionDiffusionUnsteadyMono(Fluide, bc_b, bc, Δt, Tₑ, Tᵢ, scheme)
+        if scheme == "CN"
+            s.A = build_mono_unstead_diff_moving_matrix(phase.operator, phase.capacity, phase.Diffusion_coeff, bc_b, bc, Δt, "CN")
+            s.b = build_rhs_mono_unstead_moving_diff(phase.operator, phase.source, phase.capacity, bc_b, bc, Tᵢ, Δt, 0.0, "CN")
+        else 
+            s.A = build_mono_unstead_diff_moving_matrix(phase.operator, phase.capacity, phase.Diffusion_coeff, bc_b, bc, Δt, "BE")
+            s.b = build_rhs_mono_unstead_moving_diff(phase.operator, phase.source, phase.capacity, bc_b, bc, Tᵢ, Δt, 0.0, "BE")
+        end
+        BC_border_mono!(s.A, s.b, bc_b, phase.capacity.mesh)
 
     end
 
     if err <= tol
-        println("Converged after $iter iterations with xf = $xf, error = $err")
+        println("Converged after $iter iterations with xf = $xf_new, error = $err")
     else
-        println("Reached max_iter = $max_iter with xf = $xf, error = $err")
+        println("Reached max_iter = $max_iter with xf = $xf_new, error = $err")
     end
 
     push!(s.states, s.x)
     println("Time : $(t[1])")
     println("Max value : $(maximum(abs.(s.x)))")
-    
-    for i in 2 :nt
+
+    # Reconstruct
+    spaceTimeMesh = CartesianSpaceTimeMesh(mesh, t[2:3];tag=mesh.tag)
+    body = Body((xx,t, _=0)->(xx - xf_new), (xx,)->(xx,), ((0.0,1.0),), false)
+    capacity = Capacity(body, spaceTimeMesh)
+    operator = SpaceTimeOps(capacity.A, capacity.B, capacity.V, capacity.W, (nx, 2))
+    phase = Phase(capacity, operator, (x,y,z,t)->0.0, 1.0)
+
+    s.A = build_mono_unstead_diff_moving_matrix(phase.operator, phase.capacity, phase.Diffusion_coeff, bc_b, bc, Δt, scheme)
+    s.b = build_rhs_mono_unstead_moving_diff(phase.operator, phase.source, phase.capacity, bc_b, bc, s.x, Δt, t[2], scheme)
+    BC_border_mono!(s.A, s.b, bc_b, capacity.mesh)
+
+    err = Inf
+    old_xf = xf_new
+    iter = 0
+
+    # Second time step : Newton to compute the interface position xf2
+    while (iter < max_iter) && (err > tol)
+        iter += 1
+
+        # 1) Solve the linear system
+        if method == \
+            A_reduced, b_reduced, rows_idx, cols_idx = remove_zero_rows_cols!(s.A, s.b)
+            x_reduced = A_reduced \ b_reduced
+            s.x = zeros(size(s.A, 1))
+            s.x[cols_idx] .= x_reduced
+        else
+            log = get(kwargs, :log, false)
+            if log
+                s.x, s.ch = method(s.A, s.b; kwargs...)
+            else
+                s.x = method(s.A, s.b; kwargs...)
+            end
+        end
+        Tᵢ = s.x
+
+        # 2) Update volumes / compute new interface
+        Vn_1 = phase.capacity.A[2][1:end÷2, 1:end÷2]
+        Vn   = phase.capacity.A[2][end÷2+1:end, end÷2+1:end]
+        Hₙ   = sum(diag(Vn))
+        Hₙ₊₁ = sum(diag(Vn_1))
+
+        # Compute flux
+        W! = phase.operator.Wꜝ[1:nx, 1:nx]
+        G = phase.operator.G[1:nx, 1:nx]
+        H = phase.operator.H[1:nx, 1:nx]
+        V = phase.operator.V[1:nx, 1:nx]
+        Tₒ, Tᵧ = Tᵢ[1:nx], Tᵢ[nx+1:end]
+        Interface_term = H' * W! * G * Tₒ + H' * W! * H * Tᵧ
+        Interface_term = -1/(ρ*L) * sum(Interface_term)
+
+        # New interface position
+        res = Hₙ₊₁ - Hₙ - Interface_term
+        global xf_new = Hₙ + Interface_term
+        err = abs(xf_new - old_xf)
+        println("Iteration $iter | xf = $xf_new | error = $err")
+
+        # Store residuals
+        push!(residuals, err)
+
+        # 3) Update geometry if not converged
+        if err <= tol
+            break
+        end
+        old_xf = xf_new
+
+        # Store tn+1 and tn
+        tn1 = t[3]
+        tn  = t[2]
+
+        # 4) Rebuild domain : # Add t interpolation : x - (xf*(tn1 - t)/(\Delta t) + xff*(t - tn)/(\Delta t))
+        body = Body((xx,t, _=0)->(xx - (xf*(tn1 - t)/Δt + xf_new*(t - tn)/Δt)), (xx,)->(xx,), ((0.0,1.0),), false)
+        spaceTimeMesh = phase.capacity.mesh
+        spaceTimeMesh.tag = mesh.tag
+        
+        capacity = Capacity(body, spaceTimeMesh)
+        operator = SpaceTimeOps(capacity.A, capacity.B, capacity.V, capacity.W, (nx, 2))
+        phase   = Phase(capacity, operator, (x,y,z,t)->0.0, 1.0)
+
+        if scheme == "CN"
+            s.A = build_mono_unstead_diff_moving_matrix(phase.operator, phase.capacity, phase.Diffusion_coeff, bc_b, bc, Δt, "CN")
+            s.b = build_rhs_mono_unstead_moving_diff(phase.operator, phase.source, phase.capacity, bc_b, bc, Tᵢ, Δt, 0.0, "CN")
+        else 
+            s.A = build_mono_unstead_diff_moving_matrix(phase.operator, phase.capacity, phase.Diffusion_coeff, bc_b, bc, Δt, "BE")
+            s.b = build_rhs_mono_unstead_moving_diff(phase.operator, phase.source, phase.capacity, bc_b, bc, Tᵢ, Δt, 0.0, "BE")
+        end
+
+        BC_border_mono!(s.A, s.b, bc_b, phase.capacity.mesh)
+    end
+
+    if err <= tol
+        println("Converged after $iter iterations with xf = $xf_new, error = $err")
+    else
+        println("Reached max_iter = $max_iter with xf = $xf_new, error = $err")
+    end
+
+    push!(s.states, s.x)
+    println("Time : $(t[2])")
+    println("Max value : $(maximum(abs.(s.x)))")
+
+    readline()
+
+
+
+
+
+
+
+
+
+    for i in 2:nt
         println("Time : $(t[i])")
         spaceTimeMesh = CartesianSpaceTimeMesh(mesh, t[i:i+1];tag=mesh.tag)
         capacity = Capacity(body, spaceTimeMesh)
@@ -151,108 +267,3 @@ function solve_MotionDiffusionUnsteadyMono!(s::Solver, phase::Phase, Tᵢ::Vecto
     end
 
 end
-
-
-function run_stefan_iteration!(
-    s,
-    body::Body,
-    mesh::CartesianMesh,
-    spaceTimeMesh::CartesianSpaceTimeMesh,
-    capacity::Capacity,
-    operator::SpaceTimeOps,
-    Fluide::Phase,
-    bc_b::BorderConditions,
-    bc::AbstractBoundary,
-    u0::Vector{Float64},
-    domain,
-    ρ::Float64,
-    L::Float64,
-    max_iter::Int,
-    tol::Float64,
-    Δt::Float64,
-    Tend::Float64,
-    xf::Float64
-)
-    residuals = Float64[]  # Store residual at each iteration
-    err::Float64 = Inf
-    old_xf::Float64 = xf
-    iter::Int = 0
-
-    while (iter < max_iter) && (err > tol)
-        iter += 1
-
-        # 1) Solve the linear system
-        A_reduced, b_reduced, rows_idx, cols_idx = remove_zero_rows_cols!(s.A, s.b)
-        x_reduced = A_reduced \ b_reduced
-        s.x = zeros(size(s.A, 1))
-        s.x[cols_idx] .= x_reduced
-        Tᵢ = s.x
-
-        @show Tᵢ
-        # 2) Update volumes / compute new interface
-        Vn_1 = capacity.A[2][1:end÷2, 1:end÷2]
-        Vn   = capacity.A[2][end÷2+1:end, end÷2+1:end]
-        Hₙ   = sum(diag(Vn))
-        Hₙ₊₁ = sum(diag(Vn_1))
-
-        # Compute flux
-        nx = Fluide.operator.size[1]
-        W! = operator.Wꜝ[1:nx, 1:nx]
-        G = operator.G[1:nx, 1:nx]
-        H = operator.H[1:nx, 1:nx]
-        V = operator.V[1:nx, 1:nx]
-        Tₒ, Tᵧ = Tᵢ[1:nx], Tᵢ[nx+1:end]
-        Interface_term = H' * W! * G * Tₒ + H' * W! * H * Tᵧ
-        Interface_term = -1/(ρ*L) * sum(Interface_term)
-        @show Interface_term
-        # New interface position
-        res = Hₙ₊₁ - Hₙ - Interface_term
-        @show -diag(Vn_1 - Vn) * ρ * L - H' * W! * G * Tₒ + H' * W! * H * Tᵧ
-        @show res
-        @show Hₙ₊₁ - Hₙ
-        xf_new = Hₙ + Interface_term
-        
-        err = abs(xf_new - old_xf)
-        println("Iteration $iter | xf = $xf_new | error = $err")
-
-        # Store residuals
-        push!(residuals, err)
-
-        # 3) Update geometry if not converged
-        if err <= tol
-            break
-        end
-        old_xf = xf_new
-
-        # Store tn+1 and tn
-        tn1 = t[2]
-        tn  = t[1]
-
-        # 4) Rebuild domain : # Add t interpolation : x - (xf*(tn1 - t)/(\Delta t) + xff*(t - tn)/(\Delta t))
-        body = Body((xx,t, _=0)->(xx - (xf*(tn1 - t)/Δt + xf_new*(t - tn)/Δt)), (xx,)->(xx,), domain, false)
-        #identify!(mesh, body)
-        spaceTimeMesh.tag = mesh.tag
-
-        capacity = Capacity(body, spaceTimeMesh)
-        operator = SpaceTimeOps(capacity.A, capacity.B, capacity.V, capacity.W, (nx, 2))
-        Fluide   = Phase(capacity, operator, (x,y,z,t)->0.0, 1.0)
-
-        s = MotionDiffusionUnsteadyMono(Fluide, bc_b, bc, Δt, Tend, u0, "BE")
-
-        # recompute the linear system
-        A_reduced, b_reduced, rows_idx, cols_idx = remove_zero_rows_cols!(s.A, s.b)
-        x_reduced = A_reduced \ b_reduced
-        s.x = zeros(size(s.A, 1))
-        s.x[cols_idx] .= x_reduced
-        Tᵢ = s.x
-    end
-
-    if err <= tol
-        println("Converged after $iter iterations with xf = $xf, error = $err")
-    else
-        println("Reached max_iter = $max_iter with xf = $xf, error = $err")
-    end
-
-    return xf,residuals
-end
-
